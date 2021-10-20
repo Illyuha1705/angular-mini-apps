97a72f7a79db575efd1ccc36e09d64d9986a6404
Illyuha1705 <lotvin.2002@gmail.com>
2021-10-20 20:26:04 +0300

    feat 1.1 [core3] LCP-237: added component header, app background

--- weather-app/src/app/components/weather/weather.module.ts
+++ weather-app/src/app/components/weather/weather.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from "@angular/core";
 
 import {WeatherComponent} from "./weather.component";
+import {HeaderComponent} from "../header/header.component";
+import {FindWeatherComponent} from "../find-weather/find-weather.component";
 
 @NgModule({
   declarations: [
-    WeatherComponent,
+    WeatherComponent, HeaderComponent, FindWeatherComponent
   ],
   imports: [],
   exports: [WeatherComponent],
