7fd98556eb40cfc93468315aca591cbc29b80019
Illyuha1705 <lotvin.2002@gmail.com>
2021-10-20 21:15:03 +0300

    feat 1.3 [core3] LCP-237: added two components: main-widget and widget, also styled them

--- weather-app/src/app/components/weather/weather.module.ts
+++ weather-app/src/app/components/weather/weather.module.ts
@@ -3,10 +3,16 @@ import {NgModule} from "@angular/core";
 import {WeatherComponent} from "./weather.component";
 import {HeaderComponent} from "../header/header.component";
 import {FindWeatherComponent} from "../find-weather/find-weather.component";
+import {MainWidgetComponent} from "../main-widget/main-widget.component";
+import {WidgetComponent} from "../widget/widget.component";
 
 @NgModule({
   declarations: [
-    WeatherComponent, HeaderComponent, FindWeatherComponent
+    WeatherComponent,
+    HeaderComponent,
+    FindWeatherComponent,
+    MainWidgetComponent,
+    WidgetComponent
   ],
   imports: [],
   exports: [WeatherComponent],
