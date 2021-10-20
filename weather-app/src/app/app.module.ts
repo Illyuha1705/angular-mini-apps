bdd20eabea8efcfcd71132d4f398d5037f3d164a
Illyuha1705 <lotvin.2002@gmail.com>
2021-10-20 18:14:28 +0300

    feat 1.0 [core3] LCP-237: added components folder with two components(find-weather and weather), also added weather module file

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {WeatherModule} from "./components/weather/weather.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeatherModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
