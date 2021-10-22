d56d39b4f8849ebdadeefbd06b7d327d506b0570
Illyuha1705 <lotvin.2002@gmail.com>
2021-10-22 14:16:12 +0300

    feat [core3] LCP-237: changed api to get data, cleaned widget component and moved his functions to main-widget component

--- weather-app/src/app/components/main-widget/main-widget.component.ts
+++ weather-app/src/app/components/main-widget/main-widget.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {DataStorageService} from "../../services/data-storage.service";
+import {takeUntil} from "rxjs/operators";
+import {Subject} from "rxjs";
 
 @Component({
   selector: 'app-main-widget',
   templateUrl: './main-widget.component.html',
   styleUrls: ['./main-widget.component.scss']
 })
-export class MainWidgetComponent implements OnInit {
+export class MainWidgetComponent implements OnInit, OnDestroy {
+  weatherData: any;
 
-  constructor() { }
+  currentCity = '';
+  weatherTexts: string[] = [];
+  dates: string[] = [];
+  weatherImages: string[] = [];
+  minTemperatures: string[] = [];
+  maxTemperatures: string[] = [];
+
+  private destroy$: Subject<void> = new Subject();
+
+  constructor(private dataStorageService: DataStorageService) { }
 
   ngOnInit(): void {
+    this.weatherData = this.dataStorageService.weatherData;
+
+    this.dataStorageService.weatherDataChanged$
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (updatedWeatherData: any) => {
+          this.weatherData = updatedWeatherData;
+          this.retrieveWeatherData();
+        }
+      });
+  }
+
+  public retrieveWeatherData(): void {
+    this.currentCity = `${this.weatherData?.location.name}, ${this.weatherData?.location.country}`
+
+    this.dates = [
+      this.weatherData.forecast.forecastday[0].date,
+      this.weatherData.forecast.forecastday[1].date,
+      this.weatherData.forecast.forecastday[2].date
+    ];
+
+    this.weatherImages = [
+      this.weatherData.forecast.forecastday[0].day.condition.icon,
+      this.weatherData.forecast.forecastday[1].day.condition.icon,
+      this.weatherData.forecast.forecastday[2].day.condition.icon
+    ];
+
+    this.minTemperatures = [
+      this.weatherData.forecast.forecastday[0].day.mintemp_c,
+      this.weatherData.forecast.forecastday[1].day.mintemp_c,
+      this.weatherData.forecast.forecastday[2].day.mintemp_c
+    ];
+
+    this.maxTemperatures = [
+      this.weatherData.forecast.forecastday[0].day.maxtemp_c,
+      this.weatherData.forecast.forecastday[1].day.maxtemp_c,
+      this.weatherData.forecast.forecastday[2].day.maxtemp_c
+    ];
+
+    this.weatherTexts = [
+      this.weatherData.forecast.forecastday[0].day.condition.text,
+      this.weatherData.forecast.forecastday[1].day.condition.text,
+      this.weatherData.forecast.forecastday[2].day.condition.text
+    ];
+
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
--- weather-app/src/app/components/widget/widget.component.ts
+++ weather-app/src/app/components/widget/widget.component.ts
@@ -1,46 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {DataStorageService} from "../../services/data-storage.service";
-import {takeUntil} from "rxjs/operators";
-import {Subject} from "rxjs";
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-widget',
   templateUrl: './widget.component.html',
   styleUrls: ['./widget.component.scss']
 })
-export class WidgetComponent implements OnInit, OnDestroy {
-  weatherData: any;
-  minTemperature = '';
-  maxTemperature = '';
-  weather = '';
-  currentWeatherImg = '';
+export class WidgetComponent {
+  @Input() date: string;
+  @Input() image: string;
+  @Input() minTemperature: string;
+  @Input() maxTemperature: string;
+  @Input() weatherText: string;
 
-  private destroy$: Subject<void> = new Subject();
 
-  constructor(private dataStorageService: DataStorageService) { }
-
-  ngOnInit(): void {
-    this.weatherData = this.dataStorageService.weatherData;
-
-    this.dataStorageService.weatherDataChanged$
-      .pipe(takeUntil(this.destroy$))
-      .subscribe({
-        next: (updatedWeatherData: any) => {
-          this.weatherData = updatedWeatherData;
-          this.retrieveMainWeatherData();
-        }
-      })
-  }
-
-  public retrieveMainWeatherData() {
-      this.minTemperature = `${Math.round(this.weatherData?.main.temp_min - 273.15)}`;
-      this.maxTemperature = `${Math.round(this.weatherData?.main.temp_max - 273.15)}`;
-      this.weather = this.weatherData?.weather[0].description;
-      this.currentWeatherImg = `https://openweathermap.org/img/wn/${this.weatherData?.weather[0].icon}@2x.png`
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
 }
