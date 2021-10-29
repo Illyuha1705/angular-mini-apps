54d96c192f64c73b75b0d23405df074d6a287b47
Illyuha1705 <lotvin.2002@gmail.com>
2021-10-29 19:09:57 +0300

    feat [core3] LCP-238 started implementing form with ControlValueAccessor

--- visa-card/src/app/components/card/card.component.ts
+++ visa-card/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  cardForm: FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.cardForm =  new FormGroup({
+      'cardNumber': new FormControl('', [Validators.required]),
+      'month': new FormControl('', [Validators.required]),
+      'year': new FormControl('', [Validators.required]),
+      'cvv': new FormControl('', [Validators.required]),
+    })
   }
 
 }
--- visa-card/src/app/components/card/card.module.ts
+++ visa-card/src/app/components/card/card.module.ts
@@ -4,10 +4,11 @@ import { ButtonComponent } from '../button/button.component';
 import { CardBackPartComponent } from '../card-back-part/card-back-part.component';
 import { CardFrontPartComponent } from '../card-front-part/card-front-part.component';
 import { CardComponent } from './card.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [CardComponent, CustomCheckboxComponent, ButtonComponent, CardBackPartComponent, CardFrontPartComponent,],
-    imports: [],
+    imports: [ReactiveFormsModule],
     providers: [],
     exports: [CardComponent]
 })
