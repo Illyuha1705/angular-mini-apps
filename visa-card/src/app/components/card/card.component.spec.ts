5ce31009d3f4ffe2e07de243e08d5cb3e40a59f6
Illyuha1705 <lotvin.2002@gmail.com>
2021-11-09 17:59:05 +0200

    feat [core3] LCP-238 added new tests in card component

--- visa-card/src/app/components/card/card.component.spec.ts
+++ visa-card/src/app/components/card/card.component.spec.ts
@@ -6,6 +6,14 @@ import { NumbersOnly } from '../../directives/card-number.directive';
 describe('CardComponent', () => {
     let component: CardComponent;
     let fixture: ComponentFixture<CardComponent>;
+    let store = {};
+
+    const localStorageMock = {
+        getItem: (key: string): string => store[key] || null,
+        setItem: (key: string, value: string) => store[key] = value,
+        removeItem: (key: string) => delete store[key],
+        clear: () => store = {},
+    };
 
     beforeEach(
         waitForAsync(() => {
@@ -37,12 +45,21 @@ describe('CardComponent', () => {
     });
 
     it('should return transformed string', () => {
-       let string = '11354regtrt4gh54ghst5g84r';
-       let result = '1135-4454-584'
-       let stringLength = 19;
-
-       expect(component.transformInputValue(string, stringLength)).toBe(result);
+       expect(component.transformInputValue('11354regtrt4gh54ghst5g84r', 19)).toBe('1135-4454-584');
     });
 
+    it('should sent form data to LS', () => {
+       component.toggleSaveCardData();
+
+       localStorageMock.setItem('card-data', '5');
+       expect(store['card-data']).toBe('5');
+    });
+
+    it( 'should not sent form data to LS and should clear LS', () => {
+        localStorageMock.setItem('card-data', '5');
+        localStorageMock.clear();
+
+        expect(Object.keys(store).length).toBe(0);
+    });
 
 });
